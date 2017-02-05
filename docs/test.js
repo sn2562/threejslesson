0a18a3475f73ad6e9c71fed4ddc9728b04e0cf62
sn2562 <a-tomohiro@sumilab.org>
2017-02-05 19:25:13 +0900

    test02 particle from pcd

--- docs/test.js
+++ docs/test.js
@@ -1,22 +1,100 @@
-group = new THREE.Group();
-scene.add(group);//add scene
+/**
+ * Particles.js
+ */
 
-var program = function ( context ) {
-    context.beginPath();
-    context.arc( 0, 0, 0.5, 0, Math.PI*2, true );
-    context.fill();
+var init = function() {
+
+	// DOM
+	var container = document.getElementById('container');
+
+	// Scene
+	var scene = new THREE.Scene();
+
+	// Camera
+	var width  = window.innerWidth;
+	var height = window.innerHeight;
+	var fov    = 60;
+	var aspect = width / height;
+	var near   = 1;
+	var far    = 500;
+	var camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+	camera.position.set(0, 0, 50);
+
+	// Objects
+	var circleTexture   = new THREE.TextureLoader().load('https://dl.dropboxusercontent.com/u/873331/Projects/threejs/circle.png');
+	var triangleTexture = new THREE.TextureLoader().load('https://dl.dropboxusercontent.com/u/873331/Projects/threejs/triangle.png');
+	var parameters = {
+		square: { size: 10, color:0x00ffff, blending: THREE.NoBlending },
+		circle: { size: 10, color:0xf5f5f5, map: circleTexture, blending: THREE.NormalBlending, transparent: true, alphaTest: 0.9 },
+		triangle: { size: 10, color:0xffff00, map: triangleTexture, blending: THREE.NormalBlending, transparent: true, alphaTest: 0.9 }
+	};
+
+	var squaresGeometry = createGeometry(800);
+	var squaresMaterial = new THREE.PointsMaterial(parameters.square);
+	var squares         = new THREE.Points(squaresGeometry, squaresMaterial);
+
+	var trianglesGeometry = createGeometry(1000);
+	var trianglesMaterial = new THREE.PointsMaterial(parameters.triangle);
+	var triangles         = new THREE.Points(trianglesGeometry, trianglesMaterial);
+
+	var circlesGeometry = createGeometry(300);
+	var circlesMaterial = new THREE.PointsMaterial(parameters.circle);
+	var circles         = new THREE.Points(circlesGeometry, circlesMaterial);
+
+	scene.add(squares, circles, triangles);
+
+	// Renderer
+	var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+	renderer.setSize(width, height);
+	renderer.setClearColor(0xffffff, 0);
+	renderer.setPixelRatio(window.devicePixelRatio);
+	container.appendChild(renderer.domElement);
+
+	animate();
+
+	function animate() {
+		requestAnimationFrame(animate);
+		render();
+	}
+
+	function render() {
+		squares.rotation.x += .001;
+		squares.rotation.y += .0003;
+		squares.rotation.z += .0001;
+
+		circles.rotation.x += .001;
+		circles.rotation.y += .0003;
+		circles.rotation.z += .0001;
+
+		triangles.rotation.x -= .0001;
+		triangles.rotation.y -= .0002;
+		triangles.rotation.z -= .001;
+
+		renderer.render(scene, camera);
+	}
+
+	function resize() {
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+	}
+
+	function createGeometry(count) {
+		var geometry = new THREE.Geometry();
+
+		for (var i = 0; i < count; i++) {
+			var x = Math.random() * 1000 - 500;
+			var y = Math.random() * 1000 - 500;
+			var z = Math.random() * 1000 - 500;
+			var particle = new THREE.Vector3(x, y, z);
+			particle.velocity = new THREE.Vector3(0, -Math.random(), 0);
+			geometry.vertices.push(particle);
+		}
+
+		return geometry;
+	}
+
+	window.addEventListener('resize', resize, false);
 };
 
-for ( var i = 0; i < 50; i++ ) {
-    var material = new THREE.SpriteCanvasMaterial( {
-        color: Math.random() * 0x808008 + 0x808080,
-        program: program
-    } );
-    particle = new THREE.Sprite(material);
-    //                  particle.position.set(0,0,0);//位置
-    particle.position.x = Math.random() * 2000 - 1000;
-    particle.position.y = Math.random() * 2000 - 1000;
-    particle.position.z = Math.random() * 2000 - 1000;
-    particle.scale.x = particle.scale.y = Math.random() * 20 + 10;//大きさ
-    group.add(particle);
-}
+window.addEventListener('load', init, false);
+
