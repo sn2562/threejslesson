9cc8ad68fd7e6b3c15edeaf2bcd772a69dc54444
sn2562 <a-tomohiro@sumilab.org>
2017-02-12 10:49:32 +0900

    check

--- docs/js/controls/TrackballControls.js
+++ docs/js/controls/TrackballControls.js
@@ -10,7 +10,7 @@ THREE.TrackballControls = function ( object, domElement ) {
 	var _this = this;
 	var STATE = { NONE: - 1, ROTATE: 0, ZOOM: 1, PAN: 2, TOUCH_ROTATE: 3, TOUCH_ZOOM_PAN: 4 };
 
-	var printLog = false;//確認用
+	var printLog = true;//確認用
 
 	this.object = object;
 	this.domElement = ( domElement !== undefined ) ? domElement : document;
