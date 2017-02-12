42e44dc011bfdbe3af5acc6a799053b904648e2f
sn2562 <a-tomohiro@sumilab.org>
2017-02-13 04:50:01 +0900

    add load file function

--- docs/test.js
+++ docs/test.js
@@ -1,40 +1,58 @@
-//sketchlineの生成
-indices_array = [];//構成点をどうやってつなぐか
-positions_ = [];//構成点群
-colors_ = [];//構成線の色情報
+function loadSketch(data){
+	data = data.split(/\r\n|\r|\n/);// 行ごとに分割
+	//perse data
 
-var next = 0;
-for( var j=0 ; j<allData[0].depthData.points.length ; j++){//全ての線について行う
-	//最初の座標
-	var p = allData[0].depthData.points[j][0];
-	positions_.push(p.x,p.y,p.z);//最初の点を追加する
-	//最初の色
-	var rgb = allData[0].sketchData.colors[0].toArray();
-	colors_.push(rgb[0],rgb[1],rgb[2]);;//最初の色を追加する
+	var next = 0;
+	for(var i=0;i<data.length;i++){
+		var d = data[i].split(' ');
 
-	//最初の点以降を全て追加する
-	for( var k=1 ; k<allData[0].depthData.points[i].length ; k++ ){//線を構成する最後の点まで参照する
-		//座標
-		var p = allData[0].depthData.points[j][k];
-		positions_.push(p.x,p.y,p.z); //構成点を追加
+		//行の色
+		var c = new THREE.Color(parseInt(d[0]));// color 10進数
+		var rgb = c.toArray(); 
+		usersSketchData.colors.push(c);
 
-		//色
-		var rgb = allData[0].sketchData.colors[k].toArray();
-		colors_.push(rgb[0],rgb[1],rgb[2]);;//最初の色を追加する
+		//線の太さ
+		var w = parseInt(d[1]);// 太さ情報
+		usersSketchData.lineWidth.push(w);
 
-		next = next+k;
-		indices_array.push( next-1, next );//ひとつ前の登録点とつなぐように設定
+		var points = [];
+		//座標情報
+		for(var j=2 ; j<d.length ; j=j+3){
+			var px = parseFloat(d[j]);
+			var py = parseFloat(d[j+1]);
+			var pz = parseFloat(d[j+2]*-1+z0);
+			var p = new THREE.Vector3(px,py,pz);
+
+			//頂点情報の書き換え
+			var i = next*3;
+			usersSketchData.positions_[i] = p.x;
+			usersSketchData.positions_[i+1] = p.y;
+			usersSketchData.positions_[i+2] = p.z;
+
+			//線の色の書き換え
+			usersSketchData.colors_[i] = rgb[0];
+			usersSketchData.colors_[i+1] = rgb[1];
+			usersSketchData.colors_[i+2] = rgb[2];
+
+			//どの点同士をつなぐか
+
+			if( points.length != 0 )//ストロークの最初の線でなければ
+				usersSketchData.indices_array.push(next-1,next);//一つ前の点とつなぐように設定
+
+			points.push(p);
+			next++;
+		}
+		usersSketchData.vertices.push(points);
 	}
-	next++;//線の区切り添字を一つ次に飛ばして、前の線と連結されないようにする
-}
-//データごとの区切りを登録
+	usersSketchData.lines = usersSketchData.colors.length;
 
-sketchGeometry.setIndex( new THREE.BufferAttribute( new Uint16Array( indices_array ), 1 ) );
 
-//すべてのデータについて、色情報と構成点情報を追加する
-var sketchPositions = [];
-var sketchColors = [];
-for(var i=0;i<allData.length;i++){
-	sketchPositions = sketchPositions.concat(allData[i].sketchData.positions_);
-	sketchColors = sketchColors.concat(allData[i].sketchData.colors_);
+	usersSketchData.endpoints = usersSketchData.indices_array.length;
+
+	//更新
+	usGeometry.addAttribute( 'position', new THREE.BufferAttribute( usersSketchData.positions_ , 3 ) );//構成点
+	usGeometry.addAttribute( 'color', new THREE.BufferAttribute( new Float32Array( usersSketchData.colors_ ), 3 ) );//色
+	usGeometry.setIndex( new THREE.BufferAttribute( new Uint16Array( usersSketchData.indices_array ), 1 ) );//線のつなぎ方を設定
+	usGeometry.setDrawRange(0,usersSketchData.endpoints);//表示領域を決める
+	renderer.render(scene, camera);
 }
